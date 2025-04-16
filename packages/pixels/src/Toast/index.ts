f7740fbd09f8767b15b6f55d0c8a8a5d6ea0d4f3
Hannes Tiede <951466+toxsick@users.noreply.github.com>
2025-04-16 17:55:26 +0200

    fix(pixels): add correct index export for Toast

--- packages/pixels/src/Toast/index.ts
+++ packages/pixels/src/Toast/index.ts
@@ -2,11 +2,8 @@ import { addToast, toastVariants } from './addToast';
 import ToastProvider from './ToastProvider';
 
 export type { ToastProps, AddToastVariantProps } from './addToast';
-
 export type { ToastProviderProps } from './ToastProvider';
 
-export { ToastProvider };
-
-export { addToast, toastVariants };
+export { addToast, ToastProvider, toastVariants };
 
 export default ToastProvider;
