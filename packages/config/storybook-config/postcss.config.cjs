964d30f577a79abfe332f6594b541de614a67bbc
Hannes Tiede <951466+toxsick@users.noreply.github.com>
2025-07-16 17:07:09 +0200

    fix: use vite plugin insted of postcss for tailwind

--- packages/config/storybook-config/postcss.config.cjs
+++
@@ -1,5 +0,0 @@
-/* eslint-disable import/no-extraneous-dependencies */
-
-const postcssConfig = require('@repo/tailwind-config/postcss.config.cjs');
-
-module.exports = postcssConfig;
--- packages/config/vite-config/vite.config.ts
+++ packages/config/vite-config/vite.config.ts
@@ -1,10 +1,11 @@
 /* eslint-disable import/no-extraneous-dependencies */
 
+import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [viteTsconfigPaths(), react()],
+  plugins: [viteTsconfigPaths(), react(), tailwindcss()],
 });
