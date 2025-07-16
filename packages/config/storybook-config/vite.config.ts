4d024548019056b912d4870820abf2a0be9714af
Hannes Tiede <951466+toxsick@users.noreply.github.com>
2025-07-16 17:07:09 +0200

    fix: use vite plugin insted of postcss for tailwind

--- packages/config/storybook-config/vite.config.ts
+++ packages/config/storybook-config/vite.config.ts
@@ -1,10 +1,14 @@
+// INFO: has to be same as packages/config/vite-config/vite.config.ts
+// import currently fails with TypeError [ERR_UNKNOWN_FILE_EXTENSION]: Unknown file extension ".ts"
+
 /* eslint-disable import/no-extraneous-dependencies */
 
+import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [viteTsconfigPaths(), react()],
+  plugins: [viteTsconfigPaths(), react(), tailwindcss()],
 });
