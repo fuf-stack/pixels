aa72751eec4c8ab316f59a2b1b360b9a0a1384a9
Hannes Tiede <951466+toxsick@users.noreply.github.com>
2025-07-17 13:59:24 +0200

    fix: fix dark mode in storybook

--- packages/config/storybook-config/preview.tsx
+++ packages/config/storybook-config/preview.tsx
@@ -1,57 +1,35 @@
 /* eslint-disable import/no-extraneous-dependencies */
 
-import type { Decorator, Preview } from '@storybook/react-vite';
+import type { Preview } from '@storybook/react-vite';
 
-import { useEffect } from 'react';
-
-import { DARK_MODE_EVENT_NAME, useDarkMode } from '@vueless/storybook-dark-mode';
-import { UPDATE_GLOBALS } from 'storybook/internal/core-events';
-import { addons } from 'storybook/preview-api';
-
-// see: https://github.com/hipstersmoothie/storybook-dark-mode/issues/168
-const DarkModeHtmlAttributeDecorator: Decorator = (Story) => {
-  const isDarkMode = useDarkMode();
-  useEffect(() => {
-    document.documentElement.dataset.theme = isDarkMode ? 'dark' : 'light';
-  }, [isDarkMode]);
-  return <Story />;
-};
-
-// see: https://github.com/storybookjs/test-runner/issues/74#issuecomment-1165389157
-const DisableTestRunnerDecorator: Decorator = (Story, { parameters }) => {
-  if (
-    parameters.testRunner?.disable === true &&
-    navigator.userAgent.includes('StorybookTestRunner')
-  ) {
-    return <>Disabled for Test Runner</>;
-  }
-  return <Story />;
-};
-
-// see: https://storybook.js.org/docs/essentials/backgrounds#configuration
-const backgroundOptions =  {
-  lightgray: { name: 'lightgray', value: '#f4f4f5' },
-  white: { name: 'white', value: '#ffffff' },
-  dark: { name: 'dark', value: '#18181b' },
-};
+import { themes } from 'storybook/theming';
 
 const preview: Preview = {
-  decorators: [DarkModeHtmlAttributeDecorator, DisableTestRunnerDecorator],
   parameters: {
     actions: { argTypesRegex: '^on[A-Z].*' },
-    backgrounds: {
-      options: backgroundOptions,
-    },
     // show also description and default in controls panel
     controls: { expanded: true },
     // configure dark mode
-    // see: https://storybook.js.org/addons/storybook-dark-mode
+    // see: https://github.com/vuelessjs/storybook-dark-mode
+    // see: https://github.com/heroui-inc/heroui/blob/canary/packages/storybook/.storybook/preview.tsx
     darkMode: {
-      // Set the initial theme to light
-      current: 'light',
-      stylePreview: true,
+      classTarget: 'html',
+      current: 'dark',
       darkClass: 'dark',
       lightClass: 'light',
+      stylePreview: true,
+      dark: {
+        ...themes.dark,
+        appBg: '#161616',
+        barBg: 'black',
+        background: 'black',
+        appContentBg: 'black',
+        appBorderRadius: 14,
+      },
+      light: {
+        ...themes.light,
+        appBorderRadius: 14,
+      },
     },
     layout: 'centered',
   },
@@ -60,28 +38,6 @@ const preview: Preview = {
   // see: https://storybook.js.org/docs/writing-docs/autodocs#set-up-automated-documentation
   tags: ['autodocs'],
 };
-
-// change background when dark mode is toggled
-// see: https://www.bekk.christmas/post/2021/3/storybook-background-change-on-prop-change
-const channel = addons.getChannel();
-
-let previousIsDarkMode = false;
-
-const darkModeToggleListener = (isDarkMode: boolean) => {
-  if (previousIsDarkMode !== isDarkMode) {
-    console.log('dark mode changed, setting background...', {
-      isDarkMode,
-    });
-    previousIsDarkMode = isDarkMode;
-    channel.emit(UPDATE_GLOBALS, {
-      globals: {
-        backgrounds: { value: isDarkMode ? 'dark' : 'lightgray' },
-      },
-    });
-  }
-};
-
-channel.addListener(DARK_MODE_EVENT_NAME, darkModeToggleListener);
 
 export default preview;
 
