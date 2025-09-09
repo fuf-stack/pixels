bd7e8a8a9eecf081b40995d5884a2d8213b42a09
Hannes Tiede <951466+toxsick@users.noreply.github.com>
2025-09-09 15:48:32 +0200

    chore: use oklch color variables

--- packages/config/tailwind-config/plugins.ts
+++ packages/config/tailwind-config/plugins.ts
@@ -6,7 +6,7 @@ import { heroui } from '@heroui/theme';
 
 const plugins: Config['plugins'] = [
   // see: https://www.heroui.com//docs/guide/installation#tailwind-css-setup-1
-  heroui({}),
+  heroui(),
 ];
 
 export default plugins;
