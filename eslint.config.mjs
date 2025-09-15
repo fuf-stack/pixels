64e91d073c040dd344556c54359b3616a9195984
Hannes Tiede <951466+toxsick@users.noreply.github.com>
2025-09-15 23:32:38 +0200

    chore: fix eslint-config-fuf setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,12 @@
+/* eslint-disable import-x/no-extraneous-dependencies */
+
+import path from 'node:path';
+
+import { includeIgnoreFile } from '@eslint/compat';
+
 import react from '@fuf-stack/eslint-config-fuf/react';
 import vitest from '@fuf-stack/eslint-config-fuf/vitest';
 
-export default [
-  { ignores: ['dist/**', 'eslint.config.mjs', 'node_modules/**'] },
-  ...react,
-  ...vitest,
-];
+const gitignorePath = path.resolve('.', '.gitignore');
+
+export default [includeIgnoreFile(gitignorePath), ...react, ...vitest];
