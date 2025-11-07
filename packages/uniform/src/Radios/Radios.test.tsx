19f7faaf715953cfa3ed8b7c89d077c105a5aa8a
Hannes Tiede <951466+toxsick@users.noreply.github.com>
2025-11-07 16:37:56 +0100

    test(uniform): enable all skipped tests again

--- packages/uniform/src/Radios/Radios.test.tsx
+++ packages/uniform/src/Radios/Radios.test.tsx
@@ -4,12 +4,6 @@ import storySnapshots from '@repo/storybook-config/story-snapshots';
 
 import * as stories from './Radios.stories';
 
-// TODO: RadioTabs story throws after heroui v2.7 update:
-// Error: Maximum update depth exceeded. This can happen when a component repeatedly calls setState inside componentWillUpdate or componentDidUpdate.
-// React limits the number of nested updates to prevent infinite loops.
-// @ts-expect-error ok for now
-delete stories.RadioTabs;
-
 describe('Story Snapshots', () => {
   storySnapshots(stories);
 });
