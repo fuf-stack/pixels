1608d145e73c75ccb336e84251fa89778942785d
Hannes Tiede <951466+toxsick@users.noreply.github.com>
2026-01-20 20:24:56 +0100

    fix(pixel-utils): use tsup for bundling again

--- packages/pixel-utils/tsdown.config.ts
+++ packages/pixel-utils/tsdown.config.ts
@@ -1,4 +1,5 @@
+// TODO: this is not used for now, but we should use it in the future
+
 /* eslint-disable no-restricted-exports */
 /* eslint-disable import-x/no-extraneous-dependencies */
 export { default } from '@repo/tsdown-config/config';
-
