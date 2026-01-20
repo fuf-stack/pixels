951cc8b6c4348b312160cc596b89205db25132f4
Hannes Tiede <951466+toxsick@users.noreply.github.com>
2026-01-20 20:29:38 +0100

    Revert "fix(pixel-utils): use tsup for bundling again"
    This reverts commit 1608d145e73c75ccb336e84251fa89778942785d.

--- packages/pixel-utils/tsdown.config.ts
+++ packages/pixel-utils/tsdown.config.ts
@@ -1,5 +1,4 @@
-// TODO: this is not used for now, but we should use it in the future
-
 /* eslint-disable no-restricted-exports */
 /* eslint-disable import-x/no-extraneous-dependencies */
 export { default } from '@repo/tsdown-config/config';
+
