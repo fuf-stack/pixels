53fb885102dd15359dcb6279f0cfaf224776af7e
Hannes Tiede <951466+toxsick@users.noreply.github.com>
2026-05-19 21:11:39 +0200

    fix: remove vite-tsconfig-paths from vite config + @fuf-stack/vitest-config v5

--- packages/config/vite-config/vite.config.ts
+++ packages/config/vite-config/vite.config.ts
@@ -1,9 +1,13 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-import viteTsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [viteTsconfigPaths(), react(), tailwindcss()],
+  plugins: [react(), tailwindcss()],
+
+  // Automatically resolve TypeScript path mappings from tsconfig.json
+  resolve: {
+    tsconfigPaths: true,
+  },
 });
